5c71be8d6928c01dec38407e85f9f3fc0a9abe2d
Luke Kaalim <lkaalim@nine.com.au>
2020-09-23 17:48:18 +1000

    Add instance role

--- infra/test/main.tf
+++ infra/test/main.tf
@@ -23,10 +23,41 @@ resource "aws_elastic_beanstalk_application" "api" {
   description = "The REST API Component of Astral Atlas: Wilspace"
 }
 
+resource "aws_iam_instance_profile" "api" {
+  name = "9now-tvapi_profile"
+  role = aws_iam_role.api.name
+}
+
+resource "aws_iam_role" "api" {
+  name = "wilspace-api-role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_elastic_beanstalk_environment" "api_test" {
   name                = "wildspace-api-test-env"
   application         = aws_elastic_beanstalk_application.api.name
   solution_stack_name = "64bit Amazon Linux 2 v5.2.1 running Node.js 12"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.api.id
+  }
 }
 
 output "bucket" {
