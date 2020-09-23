2bbe5d93efabf1c4fd7f357479155688d1f1d4ea
Luke Kaalim <lkaalim@nine.com.au>
2020-09-23 17:03:21 +1000

    Echo outputs

--- infra/test/main.tf
+++ infra/test/main.tf
@@ -8,11 +8,24 @@ terraform {
   }
 }
 
+resource "random_pet" "build_bucket" {}
+
 provider "aws" {
   region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "astral_atlas_builds_bucket" {
+  bucket = random_pet.build_bucket.id
 }
 
 resource "aws_elastic_beanstalk_application" "api" {
   name        = "AstralAtlas: WildspaceAPI"
   description = "The REST API Component of Astral Atlas: Wilspace"
+}
+
+output "bucket" {
+  value = aws_s3_bucket.astral_atlas_builds_bucket.arn
+}
+output "api_application" {
+  value = aws_elastic_beanstalk_application.api.arn
 }
