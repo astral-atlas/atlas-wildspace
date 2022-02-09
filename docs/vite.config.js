22e7943042e4fa71e0f0d77db73cb3d9ae23c5cd
Luke Kaalim <lukekaalim@gmail.com>
2022-02-09 11:58:44 +1100

    Build 404 page
    thats just a copy of main

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,6 +1,12 @@
 
 export default {
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        'main': 'index.html',
+        '404': '404.html'
+      }
+    }
+  },
 }
