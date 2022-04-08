421469c082cc113e89eac86b79ab22ea1c740231
Luke Kaalim <lukekaalim@gmail.com>
2022-04-08 13:59:21 +1000

    Add more public block values

--- services/modules/api/storage.tf
+++ services/modules/api/storage.tf
@@ -22,6 +22,8 @@ resource "aws_s3_bucket_public_access_block" "assets_block" {
   bucket = aws_s3_bucket.assets.id
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 resource "aws_s3_bucket_cors_configuration" "cors" {
   bucket = aws_s3_bucket.assets.id
