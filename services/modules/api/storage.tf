d8313b192bd3c50f38a73b398e22a428b8248fb3
Luke Kaalim <lukekaalim@gmail.com>
2022-04-08 13:50:51 +1000

    Destroy unused assets bucket

--- services/modules/api/storage.tf
+++ services/modules/api/storage.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_public_access_block" "assets_block" {
   block_public_policy = true
 }
 resource "aws_s3_bucket_cors_configuration" "cors" {
-  bucket = aws_s3_bucket.assets.bucket
+  bucket = aws_s3_bucket.assets.id
 
   cors_rule {
     allowed_headers = ["*"]
